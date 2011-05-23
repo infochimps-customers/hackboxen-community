65bf7fd9d1bd2ae3c13971c86a9ba6d47058aa14
Travis Dempsey <travis@infochimps.com>
2011-05-23 14:23:23 -0500

    updated pennant config naming

--- hb/sports/stats/baseball/pennant/engine/pennant_endpoint.rb
+++ hb/sports/stats/baseball/pennant/engine/pennant_endpoint.rb
@@ -2,16 +2,16 @@
 module Sports
   module Stats
     module Baseball
-      class PennantTeamStandingEndpoint < Endpoint
+      class PennantEndpoint < Endpoint
 
         extend Connection::MysqlConnection
 
-        BASEBALLPENNANT_DB = 'sports_stats_baseball'
+        BASEBALLPENNANT_DB    = 'sports_stats_baseball'
         BASEBALLPENNANT_TABLE = 'pennant_team_standings'
 
         handles('team_standings') do |params, app, responder|
-          conditions   = {
-            'team_name' => params[:team_name],
+          conditions = {
+            'team_name'   => params[:team_name],
             'record_year' => params[:record_year]
           }
           raw_results = mysql_db.get_row_as_hash(BASEBALLPENNANT_DB, BASEBALLPENNANT_TABLE, conditions) || {}
