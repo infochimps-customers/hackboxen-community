ce533931bda66325ffae80e96da7d7f577196f4f
Travis Dempsey <dempsey.travis@gmail.com>
2011-06-13 22:31:46 +0000

    census changes to get apeyeye running

--- hb/government/public/census/engine/demographics_endpoint.rb
+++ hb/government/public/census/engine/demographics_endpoint.rb
@@ -13,6 +13,7 @@ module Government
         handles('places') do |params, app, responder|
           row_path = "demo.census"
           results  = hbase_db.geo_fetch('geo_location_infochimps_place', row_path, params)
+          results
         end
 
       end
