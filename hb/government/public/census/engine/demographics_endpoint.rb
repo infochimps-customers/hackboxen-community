93d85333cc6757dec10dc8c84abf1a035f961505
Travis Dempsey <dempsey.travis@gmail.com>
2011-06-13 22:08:47 +0000

    census changes to get apeyeye running

--- hb/government/public/census/engine/demographics_endpoint.rb
+++ hb/government/public/census/engine/demographics_endpoint.rb
@@ -11,15 +11,8 @@ module Government
         self.precooked_response_type = :json
 
         handles('places') do |params, app, responder|
-          lng        = params[:lng]
-          lat        = params[:lat]
-          zoom       = params[:zoom]
-          max_pages  = (params[:_limit] || 10)
-          start_page = (params[:_from]  || 0)
-
           row_path = "demo.census"
-          hbase_db.geo_fetch('geo_location_infochimps_place', lng, lat,
-            row_path, start_page, max_pages, zoom)
+          results  = hbase_db.geo_fetch('geo_location_infochimps_place', row_path, params)
         end
 
       end
