7abd049ed9b50ced9f28341940953d6eba56edbf
Travis Dempsey <travis@infochimps.com>
2011-06-30 12:17:38 -0500

    removed lib from hackboxen-community

--- lib/hackboxen/tasks.rb
+++
@@ -1,3 +0,0 @@
-import HACKBOX_LIB_DIR + '/tasks/init.rb'
-import HACKBOX_LIB_DIR + '/tasks/icss.rb'
-import HACKBOX_LIB_DIR + '/tasks/mini.rb'
--- lib/hackboxen/utils.rb
+++
@@ -1,6 +0,0 @@
-module HackBoxen
-  autoload :Config,          'hackboxen/utils/config'
-  autoload :ConfigValidator, 'hackboxen/utils/config'
-  autoload :Paths,           'hackboxen/utils/paths'
-  autoload :LocalFileSystem, 'hackboxen/utils/filesystems'
-end
